d2a410a03e632b6e74dca99f4dd03f6341b58f22
Miguel Ferre <miguelferreporcel@gmail.com>
2023-04-20 20:37:51 +0200

    Posts

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-import { Express } from "express";
+import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import mongoose, { connect } from "mongoose";
 import cors from 'cors';
+
+const app = express()
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
+app.use(cors())
+
+const CONNECTION_URL= 'mongodb+srv://miguelferreporcel:alikindoi112358@alikindoimemories.byzdufu.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000
+
+mongoose-connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message))
 
