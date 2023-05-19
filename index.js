8731a1951003789ce4cea97e78926482b58e9513
Miguel Ferre <miguelferreporcel@gmail.com>
2023-05-19 10:44:41 +0200

    memories

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
+import dotenv from "dotenv";
+
+
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose, { connect } from "mongoose";
-import cors from 'cors';
+import cors from "cors";
 
-const app = express()
+dotenv.config();
+const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true}))
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
-app.use(cors())
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-const CONNECTION_URL= 'mongodb+srv://miguelferreporcel:alikindoi112358@alikindoimemories.byzdufu.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
 
-mongoose-connect(CONNECTION_URL)
-  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-  .catch((error) => console.log(error.message))
-
+mongoose -
+    connect(CONNECTION_URL)
+        .then(() =>
+            app.listen(PORT, () =>
+                console.log(`Server running on port: ${PORT}`)
+            )
+        )
+        .catch((error) => console.log(error.message));
